e674f2502fc9794120068d8a9f7d73be81b8aff7
__BEAST__ <mihailkalacev9@gmail.com>
2023-04-11 11:31:16 +0300

    finished 2 task

--- start-hooks/src/app/components/examples/memo/memoWithUseCallbackExample.jsx
+++ start-hooks/src/app/components/examples/memo/memoWithUseCallbackExample.jsx
@@ -1,5 +1,26 @@
-const MemoWithUseCallbackExample = (props) => {
-    return null;
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+
+const LogOutButton = ({ logOut }) => {
+    useEffect(() => {
+        console.log("button logOut");
+    });
+    return (
+        <button className="btn btn-primary" onClick={logOut}>
+            LogOut
+        </button>
+    );
+};
+
+LogOutButton.propTypes = {
+    logOut: PropTypes.func
+};
+
+const MemoWithUseCallbackExample = () => {
+    const handleLogOut = () => {
+        localStorage.removeItem("auth");
+    };
+    return <LogOutButton logOut={handleLogOut} />;
 };
 
 export default MemoWithUseCallbackExample;
